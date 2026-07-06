5a82f86e85416c3864cc1a505a77bd90861343f0
WAGNER PORTO <41432778+wagnerliborioporto@users.noreply.github.com>
2026-07-06 17:02:43 -0300

    Corrigir caminhos de conteúdo do Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
       colors: {
@@ -27,4 +27,3 @@ export default {
   },
   plugins: [],
 }
-
